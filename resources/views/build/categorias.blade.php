858b4814d9afafe4c38efcde2654b7a3004b2d93
Elias Parada <eliasparadalozano@gmail.com>
2024-04-12 21:01:39 -0600

    Imagen

--- resources/views/build/categorias.blade.php
+++ resources/views/build/categorias.blade.php
@@ -12,6 +12,7 @@
                 <h3 class="text-md font-semibold mb-2">Categorías</h3>
                 @foreach($categorias as $categoria)
                     <label class="block mb-2">
+
                         <input type="checkbox" name="categorias[]" value="{{ $categoria->id }}">
                         <span class="ml-2">{{ $categoria->nombre }}</span>
                     </label>
@@ -28,6 +29,12 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 @foreach($productos as $producto)
                     <div class="bg-white p-4 shadow-md rounded-md">
+                        @foreach (json_decode($producto->imagenes) as $index => $imagen)
+                            @if($index >= 1)
+                                @break
+                            @endif
+                            <img src="{{ asset('imagenes/' . $imagen) }}" alt="{{ $producto->nombre }}" class="w-full h-auto">
+                        @endforeach
                         <h3 class="text-lg font-semibold mb-2">{{ $producto->nombre }}</h3>
                         <p class="text-gray-600">{{ $producto->categoria->nombre }}</p>
                         <p class="text-gray-600">${{ $producto->precio_venta }}</p>
